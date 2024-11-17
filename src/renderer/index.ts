45f14d4608ea2b24dca4be982f94537c50063f48
ogreverse <glado.o.guild@gmail.com>
2024-11-17 13:44:45 +0900

    update: styleを調整

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,13 +1,13 @@
-const wordInput = document.getElementById('word') as HTMLInputElement
-const contextInput = document.getElementById('context') as HTMLInputElement
-const resultDiv = document.getElementById('result') as HTMLDivElement
-const clearButton = document.getElementById('clear') as HTMLButtonElement
-const searchButton = document.getElementById('search') as HTMLButtonElement
+const wordInput = document.getElementById('input__word') as HTMLInputElement
+const contextInput = document.getElementById('input__context') as HTMLInputElement
+const resultWrapperDiv = document.getElementById('result__wrapper') as HTMLDivElement
+const clearButton = document.getElementById('button__clear') as HTMLButtonElement
+const searchButton = document.getElementById('button__search') as HTMLButtonElement
 
 clearButton.addEventListener('click', () => {
   wordInput.value = ''
   contextInput.value = ''
-  resultDiv.textContent = ''
+  resultWrapperDiv.textContent = ''
 })
 
 searchButton.addEventListener('click', async () => {
@@ -17,14 +17,32 @@ searchButton.addEventListener('click', async () => {
     alert('ワードを入力してください')
     return
   }
-  resultDiv.textContent = '検索中...'
+  
+  resultWrapperDiv.textContent = '検索中...'
+
   try {
     const result = await window.api.fetchWordMeaning(word, context)
-    resultDiv.textContent = result
+    const resultText = document.createElement('pre')
+    resultText.classList.add('result__text')
+    
+    // width は div に納まるようにする
+    resultText.style.width = '100%'
+    resultText.style.whiteSpace = 'pre-wrap'
+    resultText.style.fontSize = '12px'
+
+    resultText.textContent = result
+    resultWrapperDiv.appendChild(resultText)
 
     // 登録ボタンを追加
+    const registerButtonWrapper = document.createElement('div')
+    registerButtonWrapper.style.display = 'flex'
+
     const registerButton = document.createElement('button')
     registerButton.textContent = '登録'
+    registerButton.classList.add('button__register')
+
+    registerButton.style.marginLeft = 'auto'
+
     registerButton.addEventListener('click', async () => {
       try {
         await window.api.registerToNotion(result)
@@ -33,8 +51,10 @@ searchButton.addEventListener('click', async () => {
         alert('Notionへの登録に失敗しました')
       }
     })
-    resultDiv.appendChild(registerButton)
+
+    registerButtonWrapper.appendChild(registerButton)
+    resultWrapperDiv.appendChild(registerButtonWrapper)
   } catch (error) {
-    resultDiv.textContent = 'エラーが発生しました'
+    resultWrapperDiv.textContent = 'エラーが発生しました'
   }
 })
